1a5153c2efc5bd5437b547d198ebe2d4f0dca811
Thomas Charlat <thomas.charlat@gmail.com>
2016-08-10 10:34:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const errors = require('lambda-errors');
+
+function req (event, cb) {
+
+	if (!event) {
+		return cb(errors.internal({
+			name: 'lambda-request : missing arguments',
+			message: '\"event\" parameter has not been provided.'
+		}));
+	}
+
+	const _req = {};
+
+	["body", "headers", "cookies", "params"/*acceptsLanguages, */]
+	.map(function (key) {
+		if (event[key]) {
+			_req[key] = event[key];
+		}
+	});
+	return cb(null, req);
+}
+
+module.exports = req;
